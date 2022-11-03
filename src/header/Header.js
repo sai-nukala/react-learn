1592329818d4ac724217f5e57dad27b509a01101
Sai Nukala <sai_nukala@epam.com>
2022-11-03 22:41:17 +0530

    lintstage config

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,22 +1,32 @@
-import React from 'react';
-import './Header.css';
+import React from 'react'
+import './Header.css'
 
 function Header() {
+  console.log('Header');
+  var unused;
   return (
-    <header className='header'>
-            <div>
-                <label className='netfilx'><b>netflix</b>roulette</label>
-                <div className='alignAddButton'><button className='addButton'>+ ADD MOVIE</button></div>
-            </div>
-            <div className='title'>
-                <h1>FIND YOUR MOVIE</h1>
-                <div>
-                    <input className='searchInput' type="text" placeholder="What do you want to watch?"></input>
-                    <button className='searchButton'>SEARCH</button>
-                </div>
-            </div>
+    <header className="header">
+      <div>
+        <label className="netfilx">
+          <b>netflix</b>roulette
+        </label>
+        <div className="alignAddButton">
+          <button className="addButton">+ ADD MOVIE</button>
+        </div>
+      </div>
+      <div className="title">
+        <h1>FIND YOUR MOVIE</h1>
+        <div>
+          <input
+            className="searchInput"
+            type="text"
+            placeholder="What do you want to watch?"
+          ></input>
+          <button className="searchButton">SEARCH</button>
+        </div>
+      </div>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
