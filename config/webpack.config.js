c3cee2869719e339456bf9e7da01523c23c30bb6
Sai Nukala <sai_nukala@epam.com>
2022-11-03 22:15:26 +0530

    first commit

---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "../src/index.jsx"),
+
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].[hash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    assetModuleFilename: "images/[hash][ext][query]"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        type: "asset/resource"
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../src/index.html")
+    })
+  ]
+};
